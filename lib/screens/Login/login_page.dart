d03dad210669826d4953962f29f6e2c67970276c
VishantPasi <vishantpasi20@gmail.com>
2026-05-29 10:50:39 +0530

    Made Changes in Login Page

--- lib/screens/Login/login_page.dart
+++ lib/screens/Login/login_page.dart
@@ -1,221 +1,89 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
   @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    TextEditingController emailController = TextEditingController();
+    TextEditingController passwordController = TextEditingController();
+
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+
+      backgroundColor: const Color(0xFFFFFFF),
+      body: Column(
+        children: [
+
+          Stack(
             children: [
-              const Spacer(),
+              Transform.translate(offset: const Offset(40,-40),child: Image.asset("assets/images/background_login3.png", width: 500,height: 400,)),
 
-              // Logo
-              Center(
-                child: Container(
-                  height: 90,
-                  width: 90,
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF2563EB),
-                    borderRadius: BorderRadius.circular(24),
-                  ),
-                  child: const Icon(
-                    Icons.cloud_rounded,
-                    color: Colors.white,
-                    size: 50,
-                  ),
+              Positioned(
+                top: 70,
+                left: 20,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Step Into,",style: GoogleFonts.playfairDisplay(color: Colors.white,fontSize: 35,fontWeight: FontWeight.bold),),
+                    Text("OverCloud",style: GoogleFonts.playfairDisplay(color: Colors.deepOrange,fontSize: 35,fontWeight: FontWeight.bold),),
+                    SizedBox(height: 10,),
+                    Text("The Cloud, Reimagined",style: GoogleFonts.urbanist(color: Colors.white,fontSize: 16,fontWeight: FontWeight.bold),),
+                    const SizedBox(height: 50,),
+
+
+                    
+
+                  ],
                 ),
-              ),
+              )
 
-              const SizedBox(height: 30),
 
-              // Title
-              const Center(
-                child: Text(
-                  "OverCloud",
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 34,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              // Subtitle
-              const Center(
-                child: Text(
-                  "Your Telegram Powered Cloud",
-                  style: TextStyle(
-                    color: Colors.white70,
-                    fontSize: 15,
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 50),
-
-              // Email Field
-              _buildTextField(
-                hint: "Email Address",
-                icon: Icons.email_outlined,
-              ),
-
-              const SizedBox(height: 18),
-
-              // Password Field
-              _buildTextField(
-                hint: "Password",
-                icon: Icons.lock_outline,
-                isPassword: true,
-              ),
-
-              const SizedBox(height: 14),
-
-              // Forgot Password
-              Align(
-                alignment: Alignment.centerRight,
-                child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Color(0xFF60A5FA),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              // Login Button
-               SizedBox(
-                width: double.infinity,
-                height: 58,
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFF2563EB),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                  ),
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 24),
-
-              // Divider
-              const Row(
-                children: const [
-                  Expanded(
-                    child: Divider(color: Colors.white24),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 12),
-                    child: Text(
-                      "OR",
-                      style: TextStyle(color: Colors.white54),
-                    ),
-                  ),
-                  Expanded(
-                    child: Divider(color: Colors.white24),
-                  ),
-                ],
-              ),
-
-              const SizedBox(height: 24),
-
-              // Google Button
-               SizedBox(
-                width: double.infinity,
-                height: 58,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.g_mobiledata,
-                    color: Colors.white,
-                    size: 32,
-                  ),
-                  label: const Text(
-                    "Continue with Google",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                    ),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Colors.white24),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18),
-                    ),
-                  ),
-                ),
-              ),
-
-              const SizedBox(height: 40),
-
-              // Bottom Text
-              Center(
-                child: TextButton(
-                  onPressed: () {},
-                  child:  RichText(
-                    text: const TextSpan(
-                      text: "Don't have an account? ",
-                      style: TextStyle(color: Colors.white60),
-                      children: [
-                        TextSpan(
-                          text: "Sign Up",
-                          style: TextStyle(
-                            color: Color(0xFF60A5FA),
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-
-              const Spacer(),
             ],
-          ),
-        ),
+
+
+          )
+          ,
+          // Container(width: width,height:10, color: Colors.white,),
+          customTextField(hint: "Email", icon: Icons.email, controller: emailController),
+          const SizedBox(height: 16),
+          customTextField(hint: "Password", icon: Icons.lock, controller: passwordController),
+          const SizedBox(height: 24),
+
+        ],
       ),
+
     );
   }
 
-  static Widget _buildTextField({
-    required String hint,
-    required IconData icon,
-    bool isPassword = false,
-  }) {
-    return TextField(
-      obscureText: isPassword,
-      style: const TextStyle(color: Colors.white),
-      decoration: InputDecoration(
-        hintText: hint,
-        hintStyle: const TextStyle(color: Colors.white54),
-        prefixIcon: Icon(icon, color: Colors.white70),
-        filled: true,
-        fillColor: const Color(0xFF1E293B),
-        contentPadding: const EdgeInsets.symmetric(vertical: 20),
-        border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(18),
-          borderSide: BorderSide.none,
+  customTextField( {
+    String? hint,
+    IconData? icon,
+    TextEditingController? controller
+}){
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: TextField(
+        controller: controller,
+        style: GoogleFonts.urbanist(color: Colors.white),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 20),
+          hintText: hint,
+          hintStyle: GoogleFonts.urbanist(color: Colors.white54),
+          prefixIcon: icon != null ? Icon(icon, color: Colors.white54) : null,
+          filled: true,
+          fillColor: Colors.white.withOpacity(0.1),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
+            borderSide: BorderSide.none,
+          ),
         ),
       ),
     );
