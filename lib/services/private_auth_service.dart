6579bb90dc6a0f75cbbc5f6d4e37b445062c4793
Vishant <vishantpasi20@gmail.com>
2026-06-26 12:59:45 +0530

    implemented the biometrics functionality

--- lib/services/private_auth_service.dart
+++ lib/services/private_auth_service.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:local_auth/local_auth.dart';
+import 'package:pinput/pinput.dart';
 
 class PrivateAuthService extends StatefulWidget {
   const PrivateAuthService({super.key});
@@ -12,7 +15,7 @@ class PrivateAuthService extends StatefulWidget {
 class _PrivateAuthServiceState extends State<PrivateAuthService> {
   final LocalAuthentication auth = LocalAuthentication();
   final String pin = "1234";
-  final TextEditingController controller = TextEditingController();
+  final TextEditingController uPIN = TextEditingController(text:"");
   bool isBiometricAvailable = false;
   bool isLoading = false;
 
@@ -24,14 +27,34 @@ class _PrivateAuthServiceState extends State<PrivateAuthService> {
 
   Future<void> _checkBiometricAvailability() async {
     try {
-       bool isAvailable = await auth.canCheckBiometrics;
-    setState(() {
-      isBiometricAvailable = isAvailable;
-    });
+      bool isAvailable = await auth.canCheckBiometrics;
+      setState(() {
+        isBiometricAvailable = isAvailable;
+      });
     } catch (e) {
       debugPrint(e.toString());
     }
-   
+  }
+
+  Future<void> _biometricAuthentication() async {
+    if(!isBiometricAvailable){
+      return;
+    }
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+    
+      bool authenticate = await auth.authenticate(localizedReason: "login Via Biometrics", biometricOnly: true);
+
+      if(authenticate){
+        print("authentication completed");
+      }
+    
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   @override
@@ -44,14 +67,255 @@ class _PrivateAuthServiceState extends State<PrivateAuthService> {
       ),
       child: Scaffold(
         backgroundColor: Color.fromRGBO(15, 15, 15, 1),
-        body: Column(children: [
-          SizedBox(height: 100,),
-          Image.asset(
-                            "assets/images/private_auth.png",
-                            width: size.width,
-                            height: 250,
-                          ),
-        ],),
-    ));
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 40),
+            Stack(
+              children: [
+                Image.asset(
+                  "assets/images/pri6.png",
+                  width: size.width,
+                  height: 200,
+                ),
+              ],
+            ),
+            Text(
+              "PRIVATE FOLDER",
+              style: GoogleFonts.urbanist(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 3,
+              ),
+            ),
+            SizedBox(height: 10),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 50.0),
+              child: Text(
+                "Your files. Your Privacy. Protected.",
+                maxLines: 2,
+                style: GoogleFonts.urbanist(
+                  color: Colors.white60,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w400,
+                  letterSpacing: 0.5,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            SizedBox(height: 20),
+
+            Container(
+              width: 200,
+              padding: const EdgeInsets.symmetric(
+                horizontal: 10.0,
+                vertical: 15,
+              ),
+              decoration: BoxDecoration(
+                color: Color.fromRGBO(25, 25, 25, 0.95),
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromRGBO(25, 25, 25, 0.945),
+                    Color.fromRGBO(19, 19, 19, 0.949),
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(15),
+                border: Border.all(color: Colors.white10),
+              ),
+              child: Column(
+                children: [
+                  Text(
+                    "Enter your PIN",
+                    style: GoogleFonts.urbanist(
+                      color: Colors.white70,
+                      fontSize: 12,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: 2,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Icon(
+                        Icons.circle_outlined,
+                        color: uPIN.text.length >= 1 ? Colors.deepOrange : Colors.white54,
+                        size: 20,
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.circle_outlined,
+                        color: uPIN.text.length >= 2 ? Colors.deepOrange : Colors.white54,
+                        size: 20,
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.circle_outlined,
+                        color: uPIN.text.length >= 3 ? Colors.deepOrange : Colors.white54,
+                        size: 20,
+                      ),
+                      SizedBox(width: 20),
+                      Icon(
+                        Icons.circle_outlined,
+                        color: uPIN.text.length == 4 ? Colors.deepOrange : Colors.white54,
+                        size: 20,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 30),
+            numberKeypad(),
+            SizedBox(height: 20),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget numberKeypad() {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            singleNumberContainer("1"),
+            SizedBox(width: 40),
+            singleNumberContainer("2"),
+            SizedBox(width: 40),
+            singleNumberContainer("3"),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            singleNumberContainer("4"),
+            SizedBox(width: 40),
+            singleNumberContainer("5"),
+            SizedBox(width: 40),
+            singleNumberContainer("6"),
+          ],
+        ),
+
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            singleNumberContainer("7"),
+            SizedBox(width: 40),
+            singleNumberContainer("8"),
+            SizedBox(width: 40),
+            singleNumberContainer("9"),
+          ],
+        ),
+        SizedBox(height: 20),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            GestureDetector(
+              onTap: () => _biometricAuthentication(),
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromRGBO(24, 24, 24, 0.941),
+                  border: Border.all(
+                    color: Colors.deepOrange.withValues(alpha: 0.9),
+                    width: 0.5,
+                  ),
+              
+                  // border: BorderDirectional(top: BorderSide(color: Colors.deepOrange,width: ),),
+                ),
+                child: Center(
+                  child: Icon(
+                    Icons.fingerprint,
+                    color: Colors.deepOrange.withValues(alpha: 0.9),
+                    size: 35,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(width: 40),
+            singleNumberContainer("0"),
+
+            SizedBox(width: 40),
+            GestureDetector(
+              onTap: () {
+                uPIN.delete();
+                print("pinnnnn: ${uPIN.text}");
+                setState(() {
+                  
+                });
+
+              },
+              child: Container(
+                width: 60,
+                height: 60,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color.fromRGBO(24, 24, 24, 0.941),
+              
+                  border: BorderDirectional(
+                    top: BorderSide(color: Colors.white10, width: 2),
+                  ),
+                ),
+                child: Center(
+                  child: FaIcon(
+                    FontAwesomeIcons.deleteLeft,
+                    color: Colors.white60,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+
+  Widget singleNumberContainer(String number) {
+    return GestureDetector(
+      onTap: () {
+        if(uPIN.text.length < 4){
+uPIN.text = uPIN.text + number;
+        }
+        
+        
+      setState(() {
+        
+      });
+        
+        print("pinnnnn: ${uPIN.text}");
+      },
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: Color.fromRGBO(24, 24, 24, 0.941),
+
+          border: BorderDirectional(
+            top: BorderSide(color: Colors.white10, width: 2),
+          ),
+        ),
+        child: Center(
+          child: Text(
+            number,
+            style: GoogleFonts.urbanist(
+              color: Colors.white70,
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+              letterSpacing: 2,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
